43af03806ede60ea14dd721c9cbb8168f3c6bfe3
Andrzej Kusiak <andrzej.kusiak.stud@pw.edu.pl>
2021-11-30 02:12:13 +0100

    PIS-21 Adding endpoint with Hello World!

---
+++ src/test/java/com/hamsterbusters/squeaker/SqueakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamsterbusters.squeaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqueakerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
