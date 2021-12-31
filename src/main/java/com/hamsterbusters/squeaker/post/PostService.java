8a7c34054e918b1ede4132f3de915f292c0c0ae7
Andrzej Kusiak <andrzej.kusiak.stud@pw.edu.pl>
2021-12-31 05:01:56 +0100

    PIS-28 Dodaję endpoint do dodawania postów

---
+++ src/main/java/com/hamsterbusters/squeaker/post/PostService.java
@@ -0,0 +1,36 @@
+package com.hamsterbusters.squeaker.post;
+
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@Service
+public class PostService {
+
+    private final PostRepository postRepository;
+    private final ModelMapper modelMapper;
+
+    public void createNewPost(CreatePostDto postDto) {
+        Post post = mapDtoToPost(postDto);
+        post.setUserId(getPrincipalFromJwtToken());
+        postRepository.save(post);
+    }
+
+    private Post mapDtoToPost(CreatePostDto postDto) {
+        Post post = modelMapper.map(postDto, Post.class);
+        post.setCreationDate(LocalDateTime.now());
+        post.setModificationDate(LocalDateTime.now());
+        return post;
+    }
+
+    private int getPrincipalFromJwtToken() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return Integer.parseInt((String) authentication.getPrincipal());
+
+    }
+}
