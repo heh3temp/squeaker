293d4f57bacf3d68ede4259114e994e45172d9a5
Andrzej Kusiak <andrzej.kusiak.stud@pw.edu.pl>
2022-01-24 02:34:03 +0100

    PIS-66 Dodaję testy spring security

--- src/test/java/com/hamsterbusters/squeaker/jwt/JwtTokenUtil.java
+++ src/test/java/com/hamsterbusters/squeaker/jwt/JwtTokenUtil.java
@@ -1,6 +1,7 @@
 package com.hamsterbusters.squeaker.jwt;
 
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -24,6 +25,33 @@ public class JwtTokenUtil {
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * jwtConfig.getTokenExpirationAfterMinutes()))
                 .signWith(secretKey)
+                .compact();
+
+        return "Bearer " + token;
+    }
+
+    public String getExpiredToken() {
+        String token = Jwts.builder()
+                .setHeaderParam("typ", "JWT")
+                .setSubject(String.valueOf(1))
+                .claim("name", "Test")
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() - 1000 * 60 * 60 * 10))
+                .signWith(secretKey)
+                .compact();
+
+        return "Bearer " + token;
+    }
+
+    public String getTokenWithInvalidSignature() {
+        String invalidSecret = "InvalidSecretInvalidSecretInvalidSecretInvalidSecretInvalidSecretInvalidSecretInvalidSecretInvalidSecret";
+        String token = Jwts.builder()
+                .setHeaderParam("typ", "JWT")
+                .setSubject(String.valueOf(1))
+                .claim("name", "Test")
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() - 1000 * 60 * 60 * 10))
+                .signWith(Keys.hmacShaKeyFor(invalidSecret.getBytes()))
                 .compact();
 
         return "Bearer " + token;
